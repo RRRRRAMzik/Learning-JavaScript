900a0cf8faccebae2c54afd33056aacf055d5024
RRRRRAMzik <75689935+RRRRRAMzik@users.noreply.github.com>
2023-06-28 21:16:09 +0700

    First part

---
+++ Part_1.js
@@ -0,0 +1,74 @@
+//Task 1
+/*
+var a = 5;
+let b = 10;
+const c = 13;
+console.log(a, b, c);
+
+a = 10;
+b = 20;
+console.log(a, b);
+*/
+
+// Task 2
+/*
+const a = 100;
+a = 20;
+console.log(a);*/
+/*
+console.log(a);
+let a = 15;
+console.log(a);*/
+/*
+console.log(a);
+var a;
+a = 10;
+console.log(a);*/
+
+// Task 3
+/*
+const productName = 'Samsung';
+let productPrice = 200;
+let productQuantity = 5;
+let resultCost = productPrice * productQuantity;
+console.log(`Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} рублей. Всего было потрачено ${resultCost} рублей`);
+*/
+
+//Task 4
+/*
+console.log(typeof 'Hello',
+typeof 10,
+typeof {},
+typeof null,
+typeof undefined,
+typeof true,
+typeof [],
+typeof (() => {}))
+*/
+
+//Task 5
+/*
+console.log(10 == 10,
+10 != 10,
+12 == 'Привет',
+10 < 11,
+12 > 20,
+true && true,
+true && false,
+false && true,
+false || true,
+true || false,
+true || true,
+10 <= 10,
+18 <= 20,
+50 >= 50,
+51 >= 50,
+{} === {},
+{} == {})*/
+
+//Task 6
+
+let a = 10;
+let b = 25;
+
+console.log(a <= b)
