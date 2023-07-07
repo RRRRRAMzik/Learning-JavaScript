83c5c3f10b1b4e56efe0685fee10e099849c9199
RRRRRAMzik <75689935+RRRRRAMzik@users.noreply.github.com>
2023-07-08 02:56:44 +0700

    Part 1.2

---
+++ Part_1.2.js
@@ -0,0 +1,19 @@
+//Task 1
+/*let a = 11;
+if (a % 2 == 0){
+    console.log("Четное");
+}
+else{
+    console.log("Нечетное");
+    a += 1;
+    console.log(a);
+}*/
+
+//Task 2
+/*for (let i = 0; i < 1000; i++) {
+    if ((i % 2 != 0) || (i % 12 == 0)){
+        console.log(i);
+    }
+}*/
+
+//Task 3
